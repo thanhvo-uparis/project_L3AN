37b8ce0bb5dee4038d9dbd3b60c6c82fe8997c53
thanh vo.u-paris <61943753+congvtc32012@users.noreply.github.com>
2021-02-10 01:36:07 +0100

    Modification sur index.html
    1. Modification la façon de faire sur 'form' au fichier index.html
    2. ajoute icon(s) (de fichier index.html)
    3.ajoute un fichier login.php pour se connecter la page à BDD phpadmin

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+   function login(){
+      $u = $_POST['username'];
+   $p = $_POST['password'];
+
+   $db = mysqli_connect("localhost", "root", "", "mazarsproject");
+
+   $sql = "select * from user where nom='$u' and motdepasse='$p'";
+
+   $rs = mysqli_query($db, $sql);
+
+   if(mysqli_num_rows($rs) > 0){
+      header("Location: contact.php");
+   }
+   else{
+       /**echo "<h2>Le nom d'utilisateur ou le mot de passe est incorrect<h2>";
+       header("localtion: ./project/index.html"); **/
+      
+       echo "<h2>Le nom d'utilisateur ou le mot de passe est incorrect<h2>";
+
+   
+   } 
+   }
+   
+?>
